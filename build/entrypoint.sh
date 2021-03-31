ef51cd361d7f67c2ba60fec1f984be6ebdf3e895
RXWatcher <jrcole@gmail.com>
2021-03-31 14:43:42 +0200

    test

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -4,23 +4,12 @@
 echo "Sleeps for 15s to initialize"
 sleep 15s
 
-while true; do
 
+while true; do  echo "Copping app-files to /data"
+  
   php /app/cli.php -plex-url="$PLEX_URL" -sections="$PLEX_SECTIONS" -sort-skip-words="$PLEX_SORT_SKIP_WORDS" -token="$PLEX_TOKEN" || break
 
   echo ""
-  if [ -f /data/* ]; then
-    echo "Removing old data"
-    rm -r /data/* || break
-  fi
-
-  echo "Copping app-files to /data"
-  cp -R /app/assets /data/ || break
-  cp -R /app/plex-data/ /data/ || break
-  cp /app/index.html /data/ || break
-
-  echo "Chmoding /data"
-  chmod 0755 -R /data/ || break
 
   echo ""
   echo "Sleep until next run in $PLEX_INTERVAL"

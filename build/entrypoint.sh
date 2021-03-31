028a55b6c34b34b9e5c7a30c6d9ec7daa3f1ae6a
RXWatcher <jrcole@gmail.com>
2021-03-31 15:51:38 +0200

    fix

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -3,12 +3,13 @@
 
 echo "Sleeps for 15s to initialize"
 sleep 15s
+mkdir -p /app/plex-data ; cd /data ; ln -s /app/plex-data
 
 if [ ! -f /data/index.html ] ; then cp /app/index.html /data/ ; cp -R /app/assets /data/ ; chmod 0755 -R /data/ ; fi
 
 while true; do  echo "Copping app-files to /data"
   
-  php /app/cli.php -data-dir="/data/plex-data" -plex-url="$PLEX_URL" -sections="$PLEX_SECTIONS" -sort-skip-words="$PLEX_SORT_SKIP_WORDS" -token="$PLEX_TOKEN" || break
+  php /app/cli.php -plex-url="$PLEX_URL" -sections="$PLEX_SECTIONS" -sort-skip-words="$PLEX_SORT_SKIP_WORDS" -token="$PLEX_TOKEN" || break
 
   echo ""
 

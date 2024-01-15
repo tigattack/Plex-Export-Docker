1d4e443b018e76679a828cc11cefe307a737553e
tigattack <10629864+tigattack@users.noreply.github.com>
2024-01-15 19:52:15 +0000

    i'm stupid

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -7,6 +7,8 @@
 [ ! -d /data/assets ] && cp -R /app/assets /data/
 [ ! -d /data/plex-data ] && mkdir /data/plex-data
 
+echo "$CRON_SCHEDULE /plex-export.sh > /proc/1/fd/1 2>&1" >/etc/crontabs/root
+
 echo "Performing first-run..."
 /plex-export.sh
 
